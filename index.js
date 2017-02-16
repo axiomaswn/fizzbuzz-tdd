f4f9c611719d2b16339a15b4f8bc682a62f1813d
YOMA <email": null>
2017-02-16 11:57:50 +0700

    npm init

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = function(num){
   var data = [[5,'buzz'],[3,'fizz'],[15,'fizzbuzz']]
   var result = num
-  data.forEach(function(x){
-    if(num%x[0]==0){
-      result = x[1]
+  data.forEach(function(data){
+    if( num % data[0] == 0 ){
+      result = data[1]
     }
   })
   return result
